8514dfa7dcea03e1c89115da8c9af797e9897500
wlgus123 <mail30019794@gmail.com>
2024-06-03 15:09:14 +0900

    RedirectTest @ModelAttribute version

--- src/main/java/hs/kr/study/RedirectTest/controller/LoginController.java
+++ src/main/java/hs/kr/study/RedirectTest/controller/LoginController.java
@@ -1,19 +1,19 @@
 package hs.kr.study.RedirectTest.controller;
 
+import hs.kr.study.RedirectTest.dto.LoginDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class LoginController {
     @PostMapping("/signup")
-    public String signup(@RequestParam("name") String name,
-                        @RequestParam("email") String email,
-                        @RequestParam("addr") String addr) {
-        System.out.println("name: " + name);
-        System.out.println("email: " + email);
-        System.out.println("addr: " + addr);
+    public String signup(LoginDTO dto) {
+        System.out.println("name: " + dto.getName());
+        System.out.println("email: " + dto.getEmail());
+        System.out.println("addr: " + dto.getAddr());
 
         return "redirect:/login";
     }
---
+++ src/main/java/hs/kr/study/RedirectTest/dto/LoginDTO.java
@@ -0,0 +1,31 @@
+package hs.kr.study.RedirectTest.dto;
+
+public class LoginDTO {
+    private String name;
+    private String email;
+    private String addr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
